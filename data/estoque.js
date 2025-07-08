84fd20d151466863194e2bd5c2f881d7d0c81c55
Raphael Silva | Via Expressa Soluções Logísticas <raphael.silva@viaexpressa.com>
2025-07-08 09:58:30 -0300

    modificaçãodos produtos em  estoque.js

--- data/estoque.js
+++ data/estoque.js
@@ -1,22 +1,22 @@
 const estoque = [
   {
-    id: 1,
-    nome: "Produto A",
-    quantidade: 100,
-    preco: 10.0
+    "id": 1,
+    "nome": "Monitor LG 24",
+    "quantidade": 10,
+    "preco": 899.99
   },
   {
-    id: 2,
-    nome: "Produto B",
-    quantidade: 50,
-    preco: 20.0
+    "id": 2,
+    "nome": "Mouse Gamer RGB",
+    "quantidade": 25,
+    "preco": 129.9
   },
   {
-    id: 3,
-    nome: "Produto C",
-    quantidade: 200,
-    preco: 5.0
+    "id": 3,
+    "nome": "Teclado Mecânico ABNT2",
+    "quantidade": 15,
+    "preco": 299.99
   }
-];
+]
 
 export default estoque;
